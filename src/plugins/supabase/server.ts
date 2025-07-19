8881ac5f6298a542cb0dd37fa508732d399df5f1
Samuel <samuelarnosaputra@gmail.com>
2025-07-20 04:35:42 +0700

    refactor(supabase): improve cookie handling in server client

--- src/plugins/supabase/server.ts
+++ src/plugins/supabase/server.ts
@@ -6,20 +6,22 @@ import { createServerClient as createServerClientFunc } from '@supabase/ssr';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Define a common interface for cookie operations
-interface CookieStore {
-  getAll: () => { name: string; value: string }[];
-  setAll: (cookies: { name: string; value: string; options?: any }[]) => void;
-}
+type AwaitedCookiesType = Awaited<ReturnType<typeof cookies>>;
 
-export const createServerClient = (cookieStore: CookieStore) => {
+export const createServerClient = (cookieStore: AwaitedCookiesType) => {
   return createServerClientFunc<Database>(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
       },
       setAll(cookiesToSet) {
-        cookieStore.setAll(cookiesToSet);
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
       },
     },
   });
